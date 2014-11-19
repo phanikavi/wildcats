1fbefd01f7015ebfa25ada6b7d686bfeaba98988
Simon Hall <simon.hall@unify.com>
2014-11-19 15:07:48 -0500

    Add tests for spare & strike

--- bowlingapp/bowlingappTests/bowlingappScoringTests.m
+++ bowlingapp/bowlingappTests/bowlingappScoringTests.m
@@ -21,27 +21,85 @@
 
     Game *aGame = [[Game alloc] init];
     [aGame playGame];
-    [aGame rollBall:5];
-    [aGame rollBall:4];
-    [aGame rollBall:6];
-    [aGame rollBall:3];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:8];
-    [aGame rollBall:1];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
-    [aGame rollBall:9];
-    [aGame rollBall:0];
+    [aGame rollBall:5 completion:^(BOOL* reset){}];
+    [aGame rollBall:4 completion:^(BOOL* reset){}];
+    [aGame rollBall:6 completion:^(BOOL* reset){}];
+    [aGame rollBall:3 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:8 completion:^(BOOL* reset){}];
+    [aGame rollBall:1 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
 
+    XCTAssertEqual([aGame calculateTotal], myTestTotal);
+}
+
+- (void)testTotalScoreWithSpare {
+    NSInteger myTestTotal = 99;
+    
+    Game *aGame = [[Game alloc] init];
+    [aGame playGame];
+    [aGame rollBall:5 completion:^(BOOL* reset){}];
+    [aGame rollBall:4 completion:^(BOOL* reset){}];
+    [aGame rollBall:6 completion:^(BOOL* reset){}];
+    [aGame rollBall:3 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:1 completion:^(BOOL* reset){}];
+    [aGame rollBall:8 completion:^(BOOL* reset){}];
+    [aGame rollBall:1 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    
+    XCTAssertEqual([aGame calculateTotal], myTestTotal);
+}
+
+- (void)testTotalScoreWithStrike {
+    NSInteger myTestTotal = 100;
+    
+    Game *aGame = [[Game alloc] init];
+    [aGame playGame];
+    [aGame rollBall:10 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:6 completion:^(BOOL* reset){}];
+    [aGame rollBall:3 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:8 completion:^(BOOL* reset){}];
+    [aGame rollBall:1 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    [aGame rollBall:9 completion:^(BOOL* reset){}];
+    [aGame rollBall:0 completion:^(BOOL* reset){}];
+    
     XCTAssertEqual([aGame calculateTotal], myTestTotal);
 }
 
